31d525d4ef2de4f9d36677420c2145b58e2a7a72
HouJishuai <137073284@qq.com>
2020-12-11 08:54:29 +0800

    Fixed the square matrix inversion error in line 36

--- demo_field_regression_njust.py
+++ demo_field_regression_njust.py
@@ -1,8 +1,9 @@
-import numpy as np
 import os
+
+import numpy as np
 from scipy.io import loadmat, savemat
+
 from utils import get_kernel_matrix, get_kernel_vector
-from sklearn.utils import shuffle
 
 class_count = 100  # 训练集中人数
 train_count = 6
@@ -18,7 +19,7 @@ DAT = [dataset['Test_book'], dataset['Test_glasses'], dataset['Test_hand'], data
 Test_book (32, 32, 3, 100)
 Test_glasses (32, 32, 3, 100)
 Test_hand (32, 32, 7, 100)
-Test_illumin (32, 32, 3, 100) 
+Test_illumin (32, 32, 3, 100)
 Test_sarf (32, 32, 3, 100)
 Train_DAT (32, 32, 6, 100)
 """
@@ -42,7 +43,7 @@ if os.path.exists(intermediate_matrix_path):
     pre_inverse = mat['pre_inverse']
 else:
     f = get_kernel_matrix(train_dat)
-    pre_inverse = np.eye(n, n) / (f + lamb * np.eye(n, n))
+    pre_inverse = np.linalg.inv(f + lamb * np.eye(n, n))
     savemat(intermediate_matrix_path, {'kernel_matrix': f, 'pre_inverse': pre_inverse})
 
 print(f'inverse finished...\n')
@@ -60,7 +61,6 @@ for j in range(count[i]):  # 测试集中同一个人照片数目
             # a[[1,2],[1,2]]在python中取出的是第一行第一列和第二行第二列元素组成的向量，而在matlab中取出的是一二行与一二列交叉处的矩阵
             f_hat = f[train_label == l, :][:, train_label == l]
             v_hat = v[train_label == l]
-            # print(f'x_hat : {x_hat.shape} f_hat : {f_hat.shape} v_hat : {v_hat.shape}')
             err[l] = x_hat.T @ f_hat @ x_hat - 2 * v_hat.T @ x_hat  # @为矩阵乘法，点乘用*
         id = np.argmin(err)
         if k == id:
